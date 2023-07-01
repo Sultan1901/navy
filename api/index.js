140028f45f1924f88a3e87c512fe8a3c97624c68
sultan901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2023-07-01 23:08:30 +0300

    N

--- api/index.js
+++ api/index.js
@@ -4,6 +4,16 @@ require('./db');
 const cors = require('cors');
 const morgan = require('morgan');
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods',  'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if ('OPTIONS' == req.method) {
+     res.sendStatus(200);
+   }
+   else {
+     next();
+   }});
 app.use(express());
 app.use(express.json());
 app.use(morgan('tiny'));
