ef6f2c56879bb0efcbd71e831f6d5da60e2d5c5a
Sultan <s.alharbi@thegarage.sa>
2024-02-19 14:10:05 +0300

    edit rout

--- api/index.js
+++
@@ -1,8 +0,0 @@
-window.requestAnimationFrame = function sul () {
-  return (
-      window.requestAnimationFrame ||
-       function (callback) {
-          window.setTimeout(callback)
-       }
-  )
-}
