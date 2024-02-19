5045c001748c4b42e8a296339c5e22b72cfb37c9
Sultan <s.alharbi@thegarage.sa>
2024-02-19 14:09:05 +0300

    edit rout

--- api/routes/router.js
+++ api/routes/router.js
@@ -4,7 +4,7 @@ const controller = require('./controller');
 const memberRouter = express.Router();
 memberRouter.post('/addMember', controller.addMember);
 memberRouter.get('/getMember', controller.getMember);
-memberRouter.get('/getOneMember', controller.getById);
+memberRouter.get('/getOne', controller.getById);
 memberRouter.delete('/deleteMember/:id', controller.deleteMember);
 memberRouter.put('/updateMember/:id', controller.updateMember);
 memberRouter.put('/updateDate', controller.resetDuty);
