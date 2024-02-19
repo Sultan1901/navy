a083583fb6569cdcdc1172a792ba93c43ded67e0
Sultan <s.alharbi@thegarage.sa>
2024-02-19 14:05:58 +0300

    edit rout

--- api/index.js
+++ api/index.js
@@ -1,16 +1,8 @@
-const express = require('express');
-const app = express();
-require('./db');
-const cors = require('cors');
-const morgan = require('morgan');
-app.use(cors());
-app.use(express());
-app.use(express.json());
-app.use(morgan('tiny'));
-const memberRouter = require('./routes/router');
-app.use(memberRouter);
-
-const PORT = 5001;
-app.listen(PORT, () => {
-  console.log(`SERVER Running ON ${PORT}`);
-});
+window.requestAnimationFrame = function sul () {
+  return (
+      window.requestAnimationFrame ||
+       function (callback) {
+          window.setTimeout(callback)
+       }
+  )
+}
